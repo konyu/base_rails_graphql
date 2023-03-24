3aae36c2747d4eb744bf2898534b696a60838148
konyu <yusukedrive@gmail.com>
2023-03-24 15:07:17 +0900

    Update: Typeの必須項目修正

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -2,10 +2,10 @@
 
 module Types
   class CommentType < Types::BaseObject
-    field :id, ID, null: false
-    field :name, String
+    field :id, ID
+    field :name, String, null: false
     field :post_id, Integer, null: false
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime
+    field :updated_at, GraphQL::Types::ISO8601DateTime
   end
 end
